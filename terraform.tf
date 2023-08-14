2682869d58ea2f4d4417434969e5d705487073cd
omar elfarsaoui <31247741+elfarsaouiomar@users.noreply.github.com>
2023-08-14 14:34:33 +0100

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,24 +1,20 @@
+
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
+    local = {
+      source = "hashicorp/local"
+      version = "2.4.0"
     }
   }
-
-  required_version = ">= 1.2.0"
 }
 
-provider "aws" {
-  region  = "us-west-2"
-}
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "local_file" "foo" {
+  content  = "foo!"
+  filename = "${path.module}/foo.bar"
 
-  tags = {
-    Name = "ExampleAppServerInstance"
+   provisioner "local-exec" {
+    command = "echo env >> private_ips.txt"
   }
-}
 
+}
